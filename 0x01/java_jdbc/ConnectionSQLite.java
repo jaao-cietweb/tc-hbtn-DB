6c0b08f76fcc3e8230ba3a9026e5156c5b826bfb
joaovg@ciandt.com <joaovg@ciandt.com>
2022-07-25 16:03:05 -0300

    Commit JDBC - ex01

---
+++ 0x01/java_jdbc/ConnectionSQLite.java
@@ -0,0 +1,8 @@
+public class ConnectionSQLite {
+    public static void main(String[] args) {
+
+    }
+    public void initConnection(){
+
+    }
+}
---
+++ 0x01/java_jdbc/sql_schema_sqlite_database_2022.sql
@@ -0,0 +1,14 @@
+-- TABLE
+CREATE TABLE "CLIENTE" (
+	"id"	int NOT NULL,
+	"nome"	varchar(50) NOT NULL,
+	"telefone"	int,
+	PRIMARY KEY("id")
+);
+ 
+-- INDEX
+ 
+-- TRIGGER
+ 
+-- VIEW
+ 
