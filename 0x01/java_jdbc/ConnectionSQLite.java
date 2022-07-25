5a720158e0ab0866dfd00d16352d8077127dbb8e
joaovg@ciandt.com <joaovg@ciandt.com>
2022-07-25 16:27:33 -0300

    Commit JDBC - ex01

--- 0x01/java_jdbc/ConnectionSQLite.java
+++ 0x01/java_jdbc/ConnectionSQLite.java
@@ -1,8 +1,17 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class ConnectionSQLite {
     public static void main(String[] args) {
-
+        initConnection();
     }
-    public void initConnection(){
-
+    public static void initConnection(){
+        try {
+            Connection connection = DriverManager.getConnection
+                    ("jdbc:sqlite:sqlite_database_2022.db");
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
     }
 }
